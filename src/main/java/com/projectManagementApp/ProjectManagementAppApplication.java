334669b7466780e3eb61c279270c0d21acd03107
william-anderson <14wianderson@gmail.com>
2018-09-11 18:44:38 -0400

    Initial commit of files

---
+++ src/main/java/com/projectManagementApp/ProjectManagementAppApplication.java
@@ -0,0 +1,12 @@
+package com.projectManagementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementAppApplication.class, args);
+	}
+}
