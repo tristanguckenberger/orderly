ec4b04601d38ce0e89db5847396d9dd0e7937521
william-anderson <14wianderson@gmail.com>
2018-10-10 23:04:15 -0400

    Added FireBase key and created example query for user data

--- src/test/java/com/orderly/OrderlyApplicationTests.java
+++ src/test/java/com/orderly/OrderlyApplicationTests.java
@@ -1,4 +1,4 @@
-package com.projectManagementApp;
+package com.orderly;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ProjectManagementAppApplicationTests {
+public class OrderlyApplicationTests {
 
 	@Test
 	public void contextLoads() {
