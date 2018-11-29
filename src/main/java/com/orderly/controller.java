18616c9bdb2f1bab65cfef926b20cd49457b416c
guckentj <guckentj@mail.uc.edu>
2018-11-29 16:01:42 -0500

    Project dto modifications in controller and project.java
    attemp at passing form input via th:field="*{name}" not working, causes 500 error. Can be fixed upon removal.

--- src/main/java/com/orderly/controller.java
+++ src/main/java/com/orderly/controller.java
@@ -1,8 +1,13 @@
 package com.orderly;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.orderly.dto.Cols;
@@ -11,6 +16,8 @@ import com.orderly.dto.Task;
 import com.orderly.service.IColumnService;
 import com.orderly.service.IProjectService;
 import com.orderly.service.ITaskService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 public class controller {
@@ -18,20 +25,23 @@ public class controller {
 	public controller() {
 		
 	}
+	
+	Logger log = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	private IProjectService projectService;
 	
 	@RequestMapping("/createBoard")
-	public String createBoard(Project project) {
-		project.setName("project1");
+	public String createBoard(@ModelAttribute Project project, BindingResult errors, Model model) {
+		
 		try {
-			projectService.save(project);
+			//projectService.save();
 		} catch (Exception e) {
+			log.error("unable to save project", e);
 			e.printStackTrace();
 			return "error";
 		}
-		return "board";
+		 return "redirect:/";
 	}
 	
 	@Autowired
@@ -47,7 +57,7 @@ public class controller {
 			e.printStackTrace();
 			return "error";
 		}
-		return "board";
+		 return "redirect:/";
 	}
 	
 	@Autowired
@@ -62,10 +72,10 @@ public class controller {
 			e.printStackTrace();
 			return "/error";
 		}
-		return "board";
+		 return "redirect:/";
 	}
 	
-	@RequestMapping("/")
+	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String home() {
 		return "home";
 	}
--- src/main/java/com/orderly/dto/Project.java
+++ src/main/java/com/orderly/dto/Project.java
@@ -1,16 +1,21 @@
 package com.orderly.dto;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="project")
 public class Project {
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="ID")
 	private int id;
+	@Column(name="NAME")
 	private String name;
 	
 	public int getId() {
